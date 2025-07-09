8df354e9d10040389eb54efd1af56a6a7727b04f
Ricky <lehmannerich@live.de>
2025-07-09 09:25:59 +0200

    removed runtime edge

--- api/upload.js
+++ api/upload.js
@@ -1,9 +1,5 @@
 import { put } from "@vercel/blob";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default async function upload(request) {
   const form = await request.formData();
   const file = form.get("file");
